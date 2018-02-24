51fe8b6d9d9ab43bb647af73d147c496c218c905
ada <1262359762@qq.com>
2018-02-24 20:23:05 +0800

    add desc

---
+++ js/mvvm.js
@@ -0,0 +1,55 @@
+function MVVM(options) {
+    this.$options = options || {};
+    var data = this._data = this.$options.data;
+    var me = this;
+
+    // 数据代理
+    // 实现 vm.xxx -> vm._data.xxx
+    Object.keys(data).forEach(function(key) {
+        me._proxyData(key);
+    });
+
+    // 初始化computed
+    this._initComputed();
+
+    // 调用observe，监测数据是否变化
+    observe(data, this);
+
+    // 编译解析指令模板
+    this.$compile = new Compile(options.el || document.body, this)
+}
+
+MVVM.prototype = {
+    $watch: function(key, cb, options) {
+        new Watcher(this, key, cb);
+    },
+    // 数据属性代理，使访问vm的属性代理为访问vm._data的属性
+    _proxyData: function(key, setter, getter) {
+        var me = this;
+        setter = setter ||
+            Object.defineProperty(me, key, {
+                configurable: false,
+                enumerable: true,
+                get: function proxyGetter() {
+                    return me._data[key];
+                },
+                set: function proxySetter(newVal) {
+                    me._data[key] = newVal;
+                }
+            });
+    },
+
+    _initComputed: function() {
+        var me = this;
+        var computed = this.$options.computed;
+        if (typeof computed === 'object') {
+            Object.keys(computed).forEach(function(key) {
+                Object.defineProperty(me, key, {
+                    get: typeof computed[key] === 'function' ?
+                        computed[key] : computed[key].get,
+                    set: function() {}
+                });
+            });
+        }
+    }
+};
---
+++ readme.md
@@ -0,0 +1,3 @@
+#MVVM 双向绑定（vue）
+##### 具体细节参考 [地址](http://www.cnblogs.com/136asdxxl/p/8448951.html)
+##### 说明：参考 [地址](https://github.com/DMQ/mvvm)，研究其代码，并根据自己的理解添加注释
